93e0c21de2d2d204d5264c4e55632d11cb571c87
Sara Cemal <saracemal@gmail.com>
2020-10-21 00:07:23 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,10 +7,9 @@
 # }
 
 def select_winner(passengers)
-  winner = ""
-  passengers.each do |suite, name|
-    if suite == :suite_a && name.start_with?("A")
-      winner = name
-    end
+winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
   end
 end
